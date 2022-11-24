e1de702a87b194775f50d346dbd61257fdef3cb8
Jirka-Jinda <jindajirka@gmail.com>
2022-11-24 12:42:33 +0100

    update

---
+++ fuzzy_set.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+class context:
+    def __init__(self, U='R', L='[0,1]'):
+        self.universum = U
+        self.degrees = L
+
+class fuzzy_set:
+    def __init__(self, list_of_tuples, context=context()):
+        self.context = context
+        self.elements = np.array(list_of_tuples)
+
+    ##### GET #####
+    def U(self):
+        return self.context.universum
+    def L(self):
+        return self.context.degrees
+    def element(self, index):
+        return self.elements[index]
+    def value(self, index):
+        return self.elements[index][0]
+    def degree(self, index):
+        return self.elements[index][1]
+
+    ##### SET #####
+    def set_value(self, index, value):
+        self.elements[index] = (value, self.degree(index))
+    def set_degree(self, index, value):
+        self.elements[index] = (self.value(index), value)
+
+    ##### FUNCS #####
+    def is_singleton(self):
+        pass
+
+    def aplha_cut(self, threshold):
+        pass
+
